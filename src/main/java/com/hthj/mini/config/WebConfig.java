d6c00ff4319d1210663bfa54bc2fa974137a1cd9
caiman <1348870256@qq.com>
2019-09-11 23:57:46 +0800

    1.0

---
+++ src/main/java/com/hthj/mini/config/WebConfig.java
@@ -0,0 +1,21 @@
+package com.hthj.mini.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Created by Admin
+ * Date 2019/9/11 23:11
+ * Description
+ */
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("index");
+        registry.addViewController("/index.html").setViewName("index");
+    }
+}
